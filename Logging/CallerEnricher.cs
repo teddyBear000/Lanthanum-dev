43dde0037e20d9338ee9e4af416ca838e7aff2e1
IhorMruchko <ihormruchko666@gmail.com>
2021-08-06 20:42:27 +0300

    Changed assembly detection

--- Logging/CallerEnricher.cs
+++ Logging/CallerEnricher.cs
@@ -3,14 +3,22 @@ using System.Linq;
 using Serilog.Core;
 using Serilog.Events;
 using System.Diagnostics;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace Logging
 {
     class CallerEnricher : ILogEventEnricher
     {
+        private List<Assembly> _assemblies = new List<Assembly>() 
+        {
+            typeof(Log).Assembly, 
+            typeof(Logger).Assembly 
+        };
+        private bool IsCalledFromLogger(Assembly assembly) => _assemblies.Contains(assembly);
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
-            var skip = 7;
+            var skip = 3;
             while (true)
             {
                 var stack = new StackFrame(skip);
@@ -21,7 +29,7 @@ namespace Logging
                 }
 
                 var method = stack.GetMethod();
-                if (method.DeclaringType.Assembly != typeof(Log).Assembly)
+                if (!IsCalledFromLogger(method.DeclaringType.Assembly))
                 {
                     var caller = $"{method.DeclaringType.FullName}.{method.Name}({string.Join(", ", method.GetParameters().Select(pi => pi.ParameterType.FullName))})";
                     logEvent.AddPropertyIfAbsent(new LogEventProperty("Caller", new ScalarValue(caller)));
