3055480c9438eaec48153471128c83a064bae233
IhorMruchko <ihormruchko666@gmail.com>
2021-08-06 19:31:19 +0300

    Implemented parsing class and method information

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -1,5 +1,4 @@
 ﻿using Serilog;
-using Serilog.Enrichers;
 using Serilog.Events;
 
 namespace Logging
@@ -7,41 +6,24 @@ namespace Logging
     public class Logger : ILogger
     {
         private Serilog.Core.Logger _logger;
-        private const string Template = "[{Timestamp:yyyy-MM-dd HH:mm:ss zzz]} [{Level:u3}] [Thread {ThreadId}] {Message:lj}{NewLine}";
-        public void Debug(string message)
-        {
-            _logger.Debug(message);
-        }
+        private const string Template = "[{Timestamp:yyyy-MM-dd HH:mm:ss zzz]} [{Level:u3}] [Thread {ThreadId}] {Message:lj} (at {Caller}){NewLine}";
+        public void Debug(string message) => _logger.Debug(message);
 
-        public void Error(string message)
-        {
-            _logger.Error(message);
-        }
+        public void Error(string message) => _logger.Error(message);
 
-        public void Fatal(string message)
-        {
-            _logger.Fatal(message);
-        }
+        public void Fatal(string message) => _logger.Fatal(message);
 
-        public void Info(string message)
-        {
-            _logger.Information(message);
-        }
+        public void Info(string message) => _logger.Information(message);
 
-        public void Warning(string message)
-        {
-            _logger.Warning(message);            
-        }
+        public void Warning(string message) => _logger.Warning(message);
 
-        public Logger(string path)
-        {
-            _logger = new LoggerConfiguration()
+        public Logger(string path) => _logger = new LoggerConfiguration()
                 .Enrich.WithThreadId()
+                .Enrich.WithCaller()
                 .MinimumLevel.Warning()
                 .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Warning, outputTemplate: Template)
                 .MinimumLevel.Verbose()
                 .WriteTo.File(path, restrictedToMinimumLevel: LogEventLevel.Verbose, outputTemplate: Template)
                 .CreateLogger();
-        }
     }
 }
