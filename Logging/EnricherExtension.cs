2ec8b1d9fa786c8c455f4c93bcfd87b8360715b3
IhorMruchko <ihormruchko666@gmail.com>
2021-08-06 19:33:23 +0300

    Added extension for serilog configuration

---
+++ Logging/EnricherExtension.cs
@@ -0,0 +1,11 @@
+﻿using Serilog;
+using Serilog.Configuration;
+
+namespace Logging
+{
+    public static class EnricherExtension
+    {
+        public static LoggerConfiguration WithCaller(this LoggerEnrichmentConfiguration enrichmentConfiguration) 
+            => enrichmentConfiguration.With<CallerEnricher>();
+    }
+}
