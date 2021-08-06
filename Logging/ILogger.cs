4e7f276480854a56984f9227d6bf74e62fb82a7d
IhorMruchko <ihormruchko666@gmail.com>
2021-08-06 18:37:20 +0300

    Implemented ILogger interface

---
+++ Logging/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Logging
+{
+    public interface ILogger
+    {
+        void Info(string message);
+
+        void Debug(string message); 
+
+        void Warning(string message);
+
+        void Error(string message);
+
+        void Fatal(string message);
+
+    }
+}
---
+++ Logging/Logger.cs
@@ -0,0 +1,51 @@
+﻿using Serilog;
+using Serilog.Events;
+using System.Diagnostics;
+using System.Linq;
+using Serilog.Configuration;
+using Serilog.Core;
+
+
+namespace Logging
+{
+    public class Logger : ILogger
+    {
+        private Serilog.Core.Logger _logger;
+        private const string Template = "[{Timestamp:yyyy-MM-dd HH:mm:ss zzz]} [{Level:u3}] [Thread {ThreadId}] {Message:lj} [{SourceContext:l}.{Caller}]{NewLine}";
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            _logger.Fatal(message);
+        }
+
+        public void Info(string message)
+        {
+            _logger.Information(message);
+        }
+
+        public void Warning(string message)
+        {
+            _logger.Warning(message);            
+        }
+
+        public Logger(string path)
+        {
+            _logger = new LoggerConfiguration()
+                .Enrich.WithThreadId()
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Warning, outputTemplate: Template)
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.File(path, restrictedToMinimumLevel: LogEventLevel.Verbose, outputTemplate: Template)
+                .CreateLogger();
+        }
+    }
+}
