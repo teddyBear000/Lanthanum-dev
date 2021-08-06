a0138a4b0c345c634a2838a9ec237f912d641d29
IhorMruchko <ihormruchko666@gmail.com>
2021-08-06 19:32:54 +0300

    Added class to track the location call

---
+++ Logging/CallerEnricher.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+using System.Linq;
+using Serilog.Core;
+using Serilog.Events;
+using System.Diagnostics;
+
+namespace Logging
+{
+    class CallerEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var skip = 7;
+            while (true)
+            {
+                var stack = new StackFrame(skip);
+                if (!stack.HasMethod())
+                {
+                    logEvent.AddPropertyIfAbsent(new LogEventProperty("Caller", new ScalarValue("<unknown method>")));
+                    return;
+                }
+
+                var method = stack.GetMethod();
+                if (method.DeclaringType.Assembly != typeof(Log).Assembly)
+                {
+                    var caller = $"{method.DeclaringType.FullName}.{method.Name}({string.Join(", ", method.GetParameters().Select(pi => pi.ParameterType.FullName))})";
+                    logEvent.AddPropertyIfAbsent(new LogEventProperty("Caller", new ScalarValue(caller)));
+                    return;
+                }
+
+                skip++;
+            }
+        }
+    }
+}
