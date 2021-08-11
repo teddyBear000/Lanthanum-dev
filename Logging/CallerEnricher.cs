47edd4d8eb4bba8a03f1331806bf78cb4b8cb9a3
IhorMruchko <ihormruchko666@gmail.com>
2021-08-11 13:25:00 +0300

    Loaded changes

--- Logging/CallerEnricher.cs
+++ Logging/CallerEnricher.cs
@@ -2,8 +2,8 @@
 using System.Linq;
 using Serilog.Core;
 using Serilog.Events;
-using System.Diagnostics;
 using System.Reflection;
+using System.Diagnostics;
 using System.Collections.Generic;
 
 namespace Logging
@@ -20,11 +20,11 @@ namespace Logging
         private string GetCallerData(MethodBase method) => $"{method.DeclaringType.FullName}.{method.Name}({string.Join(", ", method.GetParameters().Select(pi => pi.ParameterType.FullName))})";
 
         private void UpdateValues(ref int index, ref StackFrame stack) => (index, stack) = (++index, new StackFrame(index));
-
+       
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
-            var skip = 3;
-            for (var stack = new StackFrame(skip); stack.HasMethod(); UpdateValues(ref skip, ref stack))
+            var skipFirstStackFrames = 3;
+            for (var stack = new StackFrame(skipFirstStackFrames); stack.HasMethod(); UpdateValues(ref skipFirstStackFrames, ref stack))
             {
                 var method = stack.GetMethod();
                 if (IsCalledFromLogger(method)) continue;
