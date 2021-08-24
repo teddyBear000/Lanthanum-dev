3f64edf2259e8be48ba683c03b3f183f646b568d
Dmytro Balabukh <52417824+TitanMaiden@users.noreply.github.com>
2021-08-24 19:29:25 +0300

    Closes #24 - login layout (#31)
    * added login-page-layout
    
    * minor changes upd

---
+++ Lanthanum.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanthanum.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
