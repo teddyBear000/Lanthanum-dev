e0ad8c9516a697e696f2f66ea652df69778c676f
Dmytro Balabukh <demabalab@gmail.com>
2021-08-29 16:59:48 +0300

    added main structure files to work with

---
+++ Lanthanum.Web/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanthanum.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("admin/articles-list")]
+        public IActionResult ArticlesList()
+        {
+            return View("articles_list");
+        }
+    }
+}
