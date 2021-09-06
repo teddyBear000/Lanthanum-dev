645c26de60cc36858c1ac78ac94ad439d08238c1
IhorMruchko <ihormruchko666@gmail.com>
2021-09-06 20:59:37 +0300

    D&D

--- Lanthanum.Web/Controllers/AdminController.cs
+++ Lanthanum.Web/Controllers/AdminController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lanthanum.Web.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Lanthanum.Web.Controllers
 {
@@ -11,6 +14,6 @@ namespace Lanthanum.Web.Controllers
         [HttpGet]
         public IActionResult Article() => View();
         [HttpPost]
-        public IActionResult Article(string name) => Content($"{name}");
+        public IActionResult Article(Article article) => Content($"{article.Headline}");
     }
 }
---
+++ Lanthanum.Web/wwwroot/js/drag-drop.js
@@ -0,0 +1,54 @@
+const dropArea = document.querySelector('.drag-area');
+const fileTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/tif'];
+input = dropArea.querySelector('#getImage');
+
+input.addEventListener(
+    'change', function(){
+        ShowFile(this.files[0]);
+    }
+);
+
+dropArea.addEventListener(
+    "dragover",
+    (event) =>{
+        event.preventDefault();
+        dropArea.classList.add("active");
+    }
+);
+
+dropArea.addEventListener(
+    "dragleave",
+    (event) =>{
+        event.preventDefault();
+        dropArea.classList.remove("active");
+    }
+);
+
+dropArea.addEventListener(
+    "drop",
+    (event) =>{
+        event.preventDefault();
+        dropArea.classList.add("active");
+        ShowFile(event.dataTransfer.files[0]);
+    }
+);
+
+function ShowFile(file){
+    if(fileTypes.includes(file.type)){
+        console.log("if begin: value = " + input.value)
+        let fileReader = new FileReader();
+        fileReader.onload = () => {
+            dropArea.innerHTML= `<img id="inserted" src="${fileReader.result}" height=490px>`;
+        }
+        fileReader.readAsDataURL(file);
+        dropArea.classList.add("active");
+    }
+    else{
+        alert("It is not an acceptable image format");
+        dropArea.classList.remove("active");
+    }
+}
+
+function AddElement() {
+    input.click();
+}
