810c863c37fac93688ae6295f3a7c142f89ab81a
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2021-09-10 15:27:23 +0300

    Added logging (#35)
    * Added logging
    
    * Connected logger to the Lanthanum.Web

---
+++ Lanthanum.Logging/Extensions/EnricherExtension.cs
@@ -0,0 +1,11 @@
+﻿using Serilog;
+using Serilog.Configuration;
+
+namespace Lanthanum.Logging.Extensions
+{
+    public static class EnricherExtension
+    {
+        public static LoggerConfiguration WithCaller(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+            => enrichmentConfiguration.With<CallerEnricher>();
+    }
+}
