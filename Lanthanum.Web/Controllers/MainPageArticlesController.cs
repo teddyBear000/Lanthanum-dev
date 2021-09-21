5584fe5aaad421d34dda22086d3e7a6928c7b252
bohdanburda <b.burda2002@gmail.com>
2021-09-21 23:02:52 +0300

    Main Frontend

--- Lanthanum.Web/Controllers/MainPageArticlesController.cs
+++ Lanthanum.Web/Controllers/MainPageArticlesController.cs
@@ -3,19 +3,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lanthanum.Web.Data.Repositories;
+using Lanthanum.Web.Domain;
 
 namespace Lanthanum.Web.Controllers
 {
     public class MainPageArticlesController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public List<Lanthanum.Web.Domain.Article> GetMainArticles()
+        private readonly DbRepository<Article> _articleRepository;
+
+        public MainPageArticlesController(DbRepository<Article> articleRepository) 
         {
-            return new List<Lanthanum.Web.Domain.Article> { };
+            //context.AddRange(data);
+
+            _articleRepository = articleRepository;
+        }
+        public static List<Article> GetMainArticles()
+        {
+            var data = new List<Lanthanum.Web.Domain.Article> {
+                new Domain.Article {
+                    Authors = new List<Domain.User>{
+                        new Domain.User { Nickname = "nick" }
+                    },
+                    Headline = "London Games return in 2019",
+                    DateTimeOfCreation = new DateTime(2019, 09, 20),
+                    LogoPath = "/images/download2.jpg",
+                    MainText = "Register to receive the latest news on ticket sales for the four NFL London Games in 2019!"
+
+                },
+                new Domain.Article {
+                    Authors = new List<Domain.User>{
+                        new Domain.User { Nickname = "nick1" }
+                    },
+                    Headline = "London Games return in 2020",
+                    DateTimeOfCreation = new DateTime(2019, 12, 15),
+                    LogoPath = "/images/auth-basketball.jpg",
+                    MainText = "Text2"
+                },
+                new Domain.Article {
+                    Authors = new List<Domain.User>{
+                        new Domain.User { Nickname = "nick1" }
+                    },
+                    Headline = "London Games return in 2021",
+                    DateTimeOfCreation = new DateTime(2020, 01, 11),
+                    LogoPath = "/images/istockphoto-913331746-612x612.jpg",
+                    MainText = "BLABLALBA BLA bAL bBLA"
+                }
+            };
+            //var articles = _articleRepository.GetAllAsync().Result.ToList();
+            var articles = data;
+            return articles;
         }
     }
 }
--- Lanthanum.Web/wwwroot/js/site.js
+++ Lanthanum.Web/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // menu switcher
+
 function switchChannel(el) {
     // find all the elements in your channel list and loop over them
     Array.prototype.slice.call(document.querySelectorAll('ul[data-tag="channelList"] li')).forEach(function (element) {
@@ -10,4 +10,24 @@ function switchChannel(el) {
     });
     // add the selected class to the element that was clicked
     el.classList.add('selected');
+}
+
+function switchMainArticle(el) {
+
+    // find all the elements in your channel list and loop over them
+    Array.prototype.slice.call(document.querySelectorAll('ul[data-tag="articleNumberList"] li')).forEach(function (element) {
+        // remove the selected class
+        element.classList.remove('activePageNumber');
+    });
+    // add the selected class to the element that was clicked
+    el.classList.add('activePageNumber');
+
+    var index = Number(el.textContent) - 1;
+
+    $('.main-carousel').carousel(index);
+    $('.additional-carousel').carousel(index);
+    /*document.getElementById("dateCardPosition").textContent = index;
+    document.getElementById("headerCardPosition").textContent = index;
+    document.getElementById("articlePartTextCardPosition").textContent = index;
+    document.getElementById("photoKindOfSportText").textContent = index;*/
 }
